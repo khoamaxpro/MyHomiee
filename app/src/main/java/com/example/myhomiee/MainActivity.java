d193e83d8db0bd2126b6b45c18cb051341e958b6
khoa <khoa.nguyen99@hcmut.edu.vn>
2021-05-20 16:46:24 +0700

    av

--- app/src/main/java/com/example/myhomiee/MainActivity.java
+++ app/src/main/java/com/example/myhomiee/MainActivity.java
@@ -1,14 +1,47 @@
 package com.example.myhomiee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userEmail, userPassword;
+    Button btnLogin;
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        firebaseAuth = FirebaseAuth.getInstance();
+        userEmail = findViewById(R.id.editTextPhoneNumber);
+        userPassword = findViewById(R.id.editTextPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseAuth.signInWithEmailAndPassword(userEmail.getText().toString(), userPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.e("code","200");
+                        }
+                        else {
+                            Log.e("code","404");
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
