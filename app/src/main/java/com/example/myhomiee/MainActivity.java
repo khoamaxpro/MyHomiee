974d24ad74b6b16d6652546cdb05a03ce7886e5a
khoa <khoa.nguyen99@hcmut.edu.vn>
2021-05-21 14:39:14 +0700

    av

--- app/src/main/java/com/example/myhomiee/MainActivity.java
+++ app/src/main/java/com/example/myhomiee/MainActivity.java
@@ -1,29 +1,52 @@
 package com.example.myhomiee;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText userEmail, userPassword;
+    Button btnLogin;
+    FirebaseAuth firebaseAuth;
     Button btnLogIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        anhXa();
-        btnLogIn.setOnClickListener(new View.OnClickListener() {
+        firebaseAuth = FirebaseAuth.getInstance();
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        userEmail = findViewById(R.id.editTextEmail);
+        userPassword = findViewById(R.id.editTextPassword);
+        btnLogin.setOnClickListener(new View.OnClickListener()
+         {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MainHomeRoom.class);
-                startActivity(intent);
+                firebaseAuth.signInWithEmailAndPassword(userEmail.getText().toString(), userPassword.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        Intent intent = new Intent(MainActivity.this, MainHomeRoom.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Log.e("code","404");
+                    }
+                }
+            });
+
             }
         });
     }
-    void anhXa(){
-        btnLogIn = (Button) findViewById(R.id.btnLogIn);
-    }
+
 }
